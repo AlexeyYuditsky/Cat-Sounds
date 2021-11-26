2b152d04c8621b01020ba5ddb25041a539b1edb7
Alexey <alexeyyuditsky@gmail.com>
2021-11-26 18:04:48 +0700

    Create viewPager2 and adapter for viewPager2

--- app/src/main/java/com/alexeyyuditsky/catsounds/activities/activities/MainActivity.kt
+++ app/src/main/java/com/alexeyyuditsky/catsounds/activities/activities/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.alexeyyuditsky.catsounds.activities
+package com.alexeyyuditsky.catsounds.activities.activities
 
 import android.content.Context
 import android.content.SharedPreferences
@@ -8,7 +8,9 @@ import android.view.Menu
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
 import com.alexeyyuditsky.catsounds.R
+import com.alexeyyuditsky.catsounds.activities.adapters.CatFragmentStateAdapter
 import com.alexeyyuditsky.catsounds.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 const val KEY_THEME_PREFERENCES = "theme"
 const val darkTheme = 2
@@ -17,14 +19,26 @@ const val lightTheme = 1
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val sharedPreferences: SharedPreferences by lazy { getPreferences(Context.MODE_PRIVATE) }
-    private val toast by lazy { Toast.makeText(this, getText(R.string.toast_message), Toast.LENGTH_SHORT) }
+    private val toast by lazy {
+        Toast.makeText(
+            this,
+            getText(R.string.toast_message),
+            Toast.LENGTH_SHORT
+        )
+    }
     private var backPressedTime: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme()
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        binding.viewPager2.adapter = CatFragmentStateAdapter(this)
+        val listNameTabs =
+            listOf(getString(R.string.meow), getString(R.string.angry), getString(R.string.newborn))
+        TabLayoutMediator(binding.tabLayout, binding.viewPager2) { tab, position ->
+            tab.text = listNameTabs[position]
+        }.attach()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
---
+++ app/src/main/java/com/alexeyyuditsky/catsounds/activities/adapters/CatFragmentStateAdapter.kt
@@ -0,0 +1,17 @@
+package com.alexeyyuditsky.catsounds.activities.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class CatFragmentStateAdapter(fragmentActivity: FragmentActivity) :
+    FragmentStateAdapter(fragmentActivity) {
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        TODO("Not yet implemented")
+    }
+
+}
